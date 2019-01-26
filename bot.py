22bbe4cd9aa0a62ed165264dabbd7a89840ed9eb
unknown <antorack92@gmail.com>
2019-01-26 21:37:44 +0100

    Initial Commit

--- bot.py
+++ bot.py
@@ -1,10 +1,6 @@
 import telebot
+import time
 
-#main variables
-TOKEN = "766072158:AAHbGg4FynSbXDQAqLkdTfouHkjhMKvh--k"
-bot = telebot.TeleBot(TOKEN)
-
-#handlers
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+while True :
+	print("EHEHEHE")
+	time.sleep(3)
