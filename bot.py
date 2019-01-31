4be3cdf8bb3b4825fff67ce1d759688077ab7f72
unknown <antorack92@gmail.com>
2019-01-31 01:34:11 +0100

    emoji

--- bot.py
+++ bot.py
@@ -163,7 +163,6 @@ def getShoppingList(message):
 			bot.reply_to(message, "there was an exception!")
 
 
-
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
